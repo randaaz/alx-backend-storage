67a9d6887b513f52425a07ae10ef686f6201320b
randaaz <randaaziz972@gmail.com>
2024-06-15 08:24:43 -0700

    modify

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
-""" MongoDB Utility Functions """
+""" MongoDB Operations"""
+from pymongo import MongoClient
 
+if __name__ == "__main__":
+    """ Provides some stats"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
 
-def schools_by_topic(mongo_collection, topic):
-    """ Connect to MongoDB and return a collection object. """
-    documents = mongo_collection.find({"topics": topic})
-    return list(documents)
+    n_logs = nginx_collection.count_documents({})
+    print(f'{n_logs} logs')
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print('Methods:')
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f'\tmethod {method}: {count}')
+
+    status_check = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+
+    print(f'{status_check} status check')
